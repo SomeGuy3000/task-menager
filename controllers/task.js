58a0bec369fa14d49377e4e3f87a0c2331ea306f
SomeGuy3000 <lustyk12@wp.pl>
2021-07-20 15:15:06 +0200

    From now user may change main task

--- controllers/task.js
+++ controllers/task.js
@@ -1,4 +1,6 @@
-const { Task } = require('../models');
+const { Task, TaskProgress} = require('../models');
+let moment = require('moment');
+const { duration } = require('moment');
 
 
 module.exports = {
@@ -17,12 +19,17 @@ module.exports = {
             const tasks = await Task.findOne({
                 where: {
                     taskName: body.taskName
-                }});
+                }
+            });
             if (!tasks) {
                 try {
                     const tasks = await Task.create({
                         taskName: body.taskName,
                         finished: 0
+                    });
+                    console.log(tasks.id)
+                    const tasksInProgress = await TaskProgress.create({
+                        taskID: tasks.id
                     });
                     ctx.status = 201;
                     ctx.response.body = {
@@ -47,20 +54,84 @@ module.exports = {
             }
         }
         else {
-            ctx.status = 204; 
-            ctx.response.body = {
-                "message": "no content",
-                "code": 204
-            };
+            ctx.status = 204;
         }
     },
     changeMainTask: async ctx => {
+        const body = ctx.request.body;
+        if (body.taskID && body.userID){
+            const taskInProgress = await TaskProgress.findOne({
+                where: {
+                    taskID: body.taskID
+                }
+            });
+            if (taskInProgress.userID == body.userID) {
+                ctx.status = 409;
+                ctx.body = {
+                    "message": "task is already in progress",
+                    "code": 409
+                };
+            }
+            else {
+                if (taskInProgress.userID == null) {
+                    let stopTask = await stopPreviousTask(body.userID)
 
+                    if (stopTask) {
+                        await taskInProgress.update({
+                            userID: body.userID
+                        });
+                    }
+
+                    ctx.status = 201;
+                    ctx.response.body = {
+                        "message": "accept",
+                        "code": 201
+                    }; 
+                }
+                else{
+                    ctx.status = 409;
+                    ctx.body = {
+                        "message": "task is already occupied",
+                        "code": 409
+                    };
+                }
+            }
+        }
+        else {
+            ctx.status = 204;
+        }
     },
     endTask: async ctx => {
 
     },
     getCorrentTask: async ctx => {
 
+    }
+}
+async function stopPreviousTask (userID) {
+    const taskAlreadyInProgress = await TaskProgress.findOne({
+        where: {
+            userID: userID
+        }
+    });
+    if (taskAlreadyInProgress) {
+        let startEditingAt =  moment(taskAlreadyInProgress);
+        let diffInSec = moment().diff(startEditingAt, 'seconds');
+        if (taskAlreadyInProgress.duration == null) {
+            await taskAlreadyInProgress.update({
+                userID: null,
+                duration: diffInSec
+            });
+        }
+        else {
+            await taskAlreadyInProgress.update({
+                userID: null,
+                duration: (diffInSec + taskAlreadyInProgress.duration)
+            });
+        }
+        return true;
+    }
+    else {
+        return true;
     }
 }
